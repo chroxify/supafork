42ccbfb5dfd6c8adc77c4b5f00527e613898db90
chroxify <chvtodorov@gmail.com>
2023-12-16 21:24:04 +0100

    feat: init shadcn ui

---
+++ lib/utils.ts
@@ -0,0 +1,28 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+interface SWRError extends Error {
+  status: number;
+}
+
+// Fetcher function for SWR
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit,
+): Promise<JSON> {
+  const res = await fetch(input, init);
+
+  if (!res.ok) {
+    const error = await res.text();
+    const err = new Error(error) as SWRError;
+    err.status = res.status;
+    throw err;
+  }
+
+  return res.json();
+}
